2c8b5a2557a6b1a7045465ed3f9bfc069a9e6a3f
fridolinvii <carloichfridolin@gmail.com>
2024-06-26 15:29:38 +0000

    add lock

--- utils/utils.sh
+++ utils/utils.sh
@@ -15,3 +15,20 @@ if [ ! -d "$FOLDER_PATH_TMP" ]; then
   # If the folder does not exist, create it
   mkdir -p "$FOLDER_PATH_TMP"
 fi
+
+
+#########################################################
+
+LOCKFILE="$SCRIPT_DIR/tmp/signal_listening.lock"
+
+# Check if lock file exists
+if [ -e "$LOCKFILE" ] && kill -0 "$(cat $LOCKFILE)"; then
+    echo "Script is already running."
+    exit 1
+fi
+
+# Create a lock file with the current process ID
+echo $$ > "$LOCKFILE"
+
+# Ensure the lock file is removed when the script exits
+trap 'rm -f "$LOCKFILE"' EXIT
