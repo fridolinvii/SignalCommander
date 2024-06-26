6f005914c5848ed4c0c4a46b4cfa4e732288d0e6
fridolinvii <carloichfridolin@gmail.com>
2024-06-26 02:31:13 +0000

    add logfile

--- utils/latex.sh
+++ utils/latex.sh
@@ -20,8 +20,15 @@ while IFS= read -r message_line; do
   # Start the download with transmission-cli
   
   # check if "*tex" is in message_line
-  if [[ $message_line == *".tex" ]]; then
+  if [[ $message_line == *".tex"* ]]; then
       echo "Found tex message" 
+      if [[ $message_line == *"log"* ]]; then
+          send_log=1
+      fi
+
+      # Delete everything after "tex"
+      message_line=${message_line%%.tex*}
+      echo $message_line
 
       echo "Check if only one zip file is attached"
       file_count=$(ls "$ATTACHMENT"*.zip 2>/dev/null | wc -l)
@@ -35,9 +42,11 @@ while IFS= read -r message_line; do
         unzip -d $SCRIPT_DIR"/tmp/LATEX"  $ATTACHMENT*.zip
         # Compile the latex file
         cd $SCRIPT_DIR"/tmp/LATEX"
-        file_path=$(find . -name "Thesis_Carla.tex" -exec dirname {} \;)
+        file_path=$(find . -name ${message_line}.tex -exec dirname {} \;)
         if [ -z "$file_path" ]; then
           echo "No such file found."
+          signal-cli send -g "$group_id_latex" -m "[Bot] Error: File ${message_line}.tex not found."
+
         else
           echo "File found in directory: $file_path"
           cd $file_path
@@ -45,13 +54,17 @@ while IFS= read -r message_line; do
           
           #output=$(pdflatex -interaction=nonstopmode $message_line 2>&1)
           output=$(latexmk -f -pdf -interaction=nonstopmode $message_line 2>&1)
-
+          cp ${message_line}.log ${message_line}.txt
           if echo "$output" | grep -q "no output PDF file produced!"; then
               echo "A fatal error may occurred during LaTeX compilation."
-              signal-cli send -g "$group_id_latex" -m "[Bot] A fatal error may occurred during LaTeX compilation." -a "${message_line%.tex}.log"
+              signal-cli send -g "$group_id_latex" -m "[Bot] A fatal error may occurred during LaTeX compilation." -a "${message_line%.tex}.txt"
           else
               echo "LaTeX compilation completed successfully."
-              signal-cli send -g $group_id_latex -m "[Bot] Here is your PDF" -a "${message_line%.tex}.pdf"
+              if [ "$send_log" -eq 1 ]; then
+                  echo "send_log is set to 1"
+                  signal-cli send -g $group_id_latex -m "[Bot] Here is your Logfile" -a "${message_line}.txt"
+              fi
+              signal-cli send -g $group_id_latex -m "[Bot] Here is your PDF" -a "${message_line}.pdf"
           fi
         fi
         cd $SCRIPT_DIR
